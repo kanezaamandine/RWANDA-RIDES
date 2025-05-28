f3be4296a0f5f16149aa03c774bcd7999e51d746
KANEZA Amandine <kanezaa99@gmail.com>
2025-05-28 10:01:15 +0200

    Add Map component

---
+++ rwanda/src/Map.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Map = () => {
+  return (
+    <div className="map-container">
+       <iframe 
+        title ="map" 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1020805.9224118072!2d29.220993502172686!3d-1.9435281147159282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19c29654e73840e3%3A0x7490b026cbcca103!2sRwanda!5e0!3m2!1sen!2srw!4v1744813180145!5m2!1sen!2srw" >
+    </iframe>
+    </div>
+  );
+};
+
+export default Map;
